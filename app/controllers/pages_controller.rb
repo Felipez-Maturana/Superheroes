8125e94fb58e09a90990932d2222a9ccacc86637
Felipez-Maturana <felipe.maturana.g@usach.cl>
2017-06-21 20:13:42 -0400

    batman vs superman

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   def batman
   end
+  def batmanvssuperman
+  	render layout: "batmanvssuperman"
+  end
   def superman
   	render layout: "supermanlayout" 
-  	
   end
 end
